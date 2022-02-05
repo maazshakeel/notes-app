20017a737b4f0ed0c8741af74771c57433d38afa
maazshakeel <maaz.shakeel.bodla@gmail.com>
2022-02-05 13:07:03 +0500

    working on notes app

--- App.tsx
+++ App.tsx
@@ -3,6 +3,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from './src/screens/HomeScreen';
 import LoginScreen from './src/screens/LoginScreen';
+import RegisterScreen from './src/screens/RegisterScreen';
 
 const Stack = createNativeStackNavigator()
 
@@ -12,6 +13,7 @@ function App() {
       <Stack.Navigator initialRouteName="Login">
         <Stack.Screen name="Home" component={HomeScreen} options={{ headerShown: false }} />   
         <Stack.Screen name="Login" component={LoginScreen} options={{ headerShown: false }} />   
+        <Stack.Screen name="Register" component={RegisterScreen} options={{ headerShown: false }} />   
       </Stack.Navigator>
     </NavigationContainer>
   );
--- src/screens/LoginScreen.tsx
+++ src/screens/LoginScreen.tsx
@@ -1,12 +1,48 @@
-import * as React from 'react'
-import { Text, View, StyleSheet } from 'react-native'
+import React, { useState } from 'react'
+import { Text, TouchableOpacity, View, StyleSheet, TextInput, KeyboardAvoidingView } from 'react-native'
 import { useNavigation } from '@react-navigation/core' 
 
-const LoginScreen: React.FC = () => {
+const LoginScreen = ({ navigation }) => {
+
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
   return (
-    <View style={styles.container}>
-      <Text>LoginScreen</Text> 
-    </View>
+    <KeyboardAvoidingView
+      style={styles.container}
+      behavior="margin"
+    >
+      <View style={styles.inputContainer}>
+        <TextInput
+          placeholder="Email"
+          value={email}
+          onChangeText={text => setEmail(text)}
+          style={styles.input}
+        />
+        <TextInput
+          placeholder="Password"
+          value={password}
+          onChangeText={text => setPassword(text)}
+          style={styles.input}
+          secureTextEntry
+        />
+      </View>
+
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity
+          onPress={() => {}}
+          style={styles.button}
+        >
+          <Text style={styles.buttonText}>Login</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={() => navigation.navigate("Register")}
+          style={[styles.buttonRegister, styles.buttonOutline]}
+        >
+          <Text style={styles.buttonOutlineText}>Don't have account?</Text>
+        </TouchableOpacity>
+      </View>
+    </KeyboardAvoidingView>
   )
 } 
 
@@ -15,8 +51,60 @@ export default LoginScreen
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+  },
+  inputContainer: {
+    width: '80%'
+  },
+  input: {
+    backgroundColor: 'white',
+    paddingHorizontal: 15,
+    paddingVertical: 10,
+    borderRadius: 10,
+    marginTop: 5,
+  },
+  buttonContainer: {
+    width: '60%',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 40,
+  },
+  button: {
+    backgroundColor: '#0782F9',
+    width: '100%',
+    padding: 15,
+    borderRadius: 10,
+    alignItems: 'center',
+  },
+  buttonOutline: {
+    backgroundColor: 'white',
+    marginTop: 5,
+    borderColor: '#0782F9',
+    borderWidth: 2,
+  },
+  buttonRegister: {
+    backgroundColor: '#0782F9',
+    width: '85%',
+    padding: 15,
+    borderRadius: 10,
+    alignItems: 'center',
+
+  },
+  buttonText: {
+    color: 'white',
+    fontWeight: '700',
+    fontSize: 16,
+  },
+  buttonOutlineText: {
+    color: '#0782F9',
+    fontWeight: '700',
+    fontSize: 16,
+  },
+  headerText: {
+    fontSize: 28,
+    fontWeight: 'bold',
+    fontFamily: 'sans-serif'
   }
 })
 
