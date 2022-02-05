ec2b60ed058746c7aec977fac390e3d63e4a5da3
maazshakeel <maaz.shakeel.bodla@gmail.com>
2022-02-05 13:11:46 +0500

    Added a package react-native-vector-icons

--- App.tsx
+++ App.tsx
@@ -2,18 +2,14 @@ import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from './src/screens/HomeScreen';
-import LoginScreen from './src/screens/LoginScreen';
-import RegisterScreen from './src/screens/RegisterScreen';
 
 const Stack = createNativeStackNavigator()
 
-function App() {
+const App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Login">
+      <Stack.Navigator initialRouteName="Home">
         <Stack.Screen name="Home" component={HomeScreen} options={{ headerShown: false }} />   
-        <Stack.Screen name="Login" component={LoginScreen} options={{ headerShown: false }} />   
-        <Stack.Screen name="Register" component={RegisterScreen} options={{ headerShown: false }} />   
       </Stack.Navigator>
     </NavigationContainer>
   );
