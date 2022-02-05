d7c33a1cb67a6e02485d739ac1b39b4af8a7a945
maazshakeel <maaz.shakeel.bodla@gmail.com>
2022-02-06 00:15:53 +0500

    Ready to use navigation

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react'
 import { Text, View, StyleSheet } from 'react-native'
-import { useNavigation } from '@react-navigation/core' 
 
 const HomeScreen: React.FC = () => {
   return (
